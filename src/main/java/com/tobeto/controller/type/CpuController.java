04a9e6c0a50de7a630c77c69c1c0c4488989cfda
ProgramciDusunur <lerenarazl@gmail.com>
2024-03-20 16:41:19 +0300

    a update example for other main types

--- src/main/java/com/tobeto/controller/type/CpuController.java
+++ src/main/java/com/tobeto/controller/type/CpuController.java
@@ -6,14 +6,15 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tobeto.dto.SuccessResponseDTO;
-import com.tobeto.dto.type.CaseDelRequestDTO;
 import com.tobeto.dto.type.CpuAddRequestDTO;
 import com.tobeto.dto.type.CpuDelRequestDTO;
+import com.tobeto.dto.type.CpuPutRequestDTO;
 import com.tobeto.entity.type.Cpu;
 import com.tobeto.service.type.CpuService;
 
@@ -43,15 +44,27 @@ public class CpuController {
 		cpuService.createCpu(cpu);
 		return new SuccessResponseDTO();
 	}
+
 	@DeleteMapping(value = "/del", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public SuccessResponseDTO delSubType(@RequestBody CpuDelRequestDTO dto) {
 		System.out.println("Cpu Del calisiyor.");
-		
-		
+
 		cpuService.deleteCpu(dto.getId());
-		
 
 		// System.out.println(dto.getModel());
+		return new SuccessResponseDTO();
+	}
+
+	@PutMapping("/put")
+	public SuccessResponseDTO putSubType(@RequestBody CpuPutRequestDTO dto) {
+		Cpu cpu = requestMapper.map(dto, Cpu.class);
+		/*
+		 * cpu.setBrand(dto.getBrand()); cpu.setClockSpeed(dto.getClockSpeed());
+		 * cpu.setGeneration(dto.getGeneration()); cpu.setCoreCount(dto.getCoreCount());
+		 * cpu.setModel(dto.getModel()); cpu.setSeries(dto.getSeries());
+		 * cpu.setSocketType(dto.getSocketType());
+		 */
+		cpuService.updateCpu(cpu.getId(), cpu);
 		return new SuccessResponseDTO();
 	}
 }
---
+++ src/main/java/com/tobeto/dto/type/CpuPutRequestDTO.java
@@ -0,0 +1,15 @@
+package com.tobeto.dto.type;
+
+import lombok.Data;
+
+@Data
+public class CpuPutRequestDTO {
+	private int id;
+	private String brand;
+	private int clockSpeed;
+	private String socketType;
+	private String generation;
+	private String series;
+	private int coreCount;
+	private String model;
+}
